58b0e42ad99d9bae0b1a7a9a4dc38c6a918c79bb
Jeevanantham Sundaram Murugan <45613232+jeevanantham2002@users.noreply.github.com>
2020-07-04 17:50:55 -0400

    Project

---
+++ HomePage.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Test
+{
+    [Activity(Label = "Diabetes Detector")]
+    public class HomePage : AppCompatActivity, IOnMapReadyCallback
+    {
+        public TextView Result;
+        public TextView Recommendation;
+
+        public String DiabetesPrediction;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.HomePage);
+            DiabetesPrediction = Intent.GetStringExtra("Prediction" ?? "Not Got");
+            Result = FindViewById<TextView>(Resource.Id.Result);
+            Result.Text = DiabetesPrediction;
+            Recommendation = FindViewById<TextView>(Resource.Id.Recommendation);
+            ActionRecommendation();
+
+            var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+
+
+
+            // Create your application here
+        }
+        public void ActionRecommendation()
+        {
+            if(DiabetesPrediction== "You Have Diabetes")
+            {
+                Recommendation.Text = "Consult a Doctor, here are the highest rated options nearby";
+            }
+           if(DiabetesPrediction== "No Diabetes")
+            {
+                Recommendation.Text = "Please Remember to Still Schedule a Yearly Check Up";
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            CameraUpdate center =
+        CameraUpdateFactory.NewLatLng(new LatLng(40.5187,
+                                                 -74.4121));
+            CameraUpdate zoom = CameraUpdateFactory.ZoomTo(12);
+            googleMap.MoveCamera(center);
+            googleMap.AnimateCamera(zoom);
+
+            googleMap.MapType = GoogleMap.MapTypeNormal;
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+           
+
+            MarkerOptions Doctor1 = new MarkerOptions();
+            Doctor1.SetPosition(new LatLng(40.597470, -74.359500));
+            Doctor1.SetTitle("Janine Jamieson,D.O");
+            Doctor1.SetSnippet("Number:(908) 834-8343");
+            googleMap.AddMarker(Doctor1);
+
+            MarkerOptions Doctor2 = new MarkerOptions();
+            Doctor2.SetPosition(new LatLng(40.571550, -74.355140));
+            Doctor2.SetTitle("Dr. Hitesh R. Patel, MD");
+            Doctor2.SetSnippet("Number:(732) 744-0634");
+            googleMap.AddMarker(Doctor2);
+
+            MarkerOptions Doctor3 = new MarkerOptions();
+            Doctor3.SetPosition(new LatLng(40.541870, -74.394900));
+            Doctor3.SetTitle("Edison Wellness Medical Group: Hao Zhang, MD");
+            Doctor3.SetSnippet("Number:(732) 201-6985");
+            googleMap.AddMarker(Doctor3);
+
+            
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Android.Util;
+using Android.Content;
+
+namespace Test
+{
+    [Activity(Label = "Diabetes Detector", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public static string FinalResult= "hello";//tells if you have diabities or not
+        public static EditText Pregnancies;
+        public static EditText PlasmaGlucose;
+        public static EditText BloodPressure;
+        public static EditText TricepThickness;
+        public static EditText SerumInsulin;
+        public static EditText BMI;
+        public static EditText DiabitiesPedigree;
+        public static EditText Age;
+        public String PredictionString;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+             Pregnancies = FindViewById<EditText>(Resource.Id.Pregnancies);
+             PlasmaGlucose = FindViewById<EditText>(Resource.Id.PlasmaGlucose);
+             BloodPressure = FindViewById<EditText>(Resource.Id.BloodPressure);
+             TricepThickness = FindViewById<EditText>(Resource.Id.TricepThickness);
+             SerumInsulin = FindViewById<EditText>(Resource.Id.SerumInsulin);
+             BMI = FindViewById<EditText>(Resource.Id.BMI);
+             DiabitiesPedigree =FindViewById<EditText>(Resource.Id.DiabitiesPedigree);
+             Age = FindViewById<EditText>(Resource.Id.Age);
+            Button submit = FindViewById<Button>(Resource.Id.Submit);
+            TextView Result = FindViewById<TextView>(Resource.Id.Result);
+            this.FindViewById<Button>(Resource.Id.Submit).Click += this.execute;
+
+
+
+            //Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+
+        }
+        public string prediction(string serverinput)
+        {
+            if (serverinput.Contains("0"))
+            {
+                return "No Diabetes";
+            }
+
+            if (serverinput.Contains("1"))
+            {
+                return "You Have Diabetes";
+            }
+            else return "process pending try again";
+        }
+        public void execute(object sender, EventArgs args)
+        {
+            Log.Info("error", "this is an info message");
+            //Toast.MakeText(this, FinalResult, ToastLength.Long).Show();
+            InvokeRequestResponseService().Wait();
+            // this.FindViewById<TextView>(Resource.Id.Result).Text = prediction(FinalResult); // Predicts Whether you have diabtes or not
+            PredictionString = prediction(FinalResult);
+            Intent nextActivity = new Intent(this, typeof(HomePage));
+            nextActivity.PutExtra("Prediction", PredictionString);
+            StartActivity(nextActivity);
+
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>() {
+                        {
+                            "input1",
+                            new List<Dictionary<string, string>>(){new Dictionary<string, string>(){
+                                            {
+                                                "PatientID", "1354778"
+                                            },
+                                            {
+                                                "Pregnancies", Pregnancies.Text
+                                            },
+                                            {
+                                                "PlasmaGlucose", PlasmaGlucose.Text
+                                            },
+                                            {
+                                                "DiastolicBloodPressure", BloodPressure.Text
+                                            },
+                                            {
+                                                "TricepsThickness", TricepThickness.Text
+                                            },
+                                            {
+                                                "SerumInsulin", SerumInsulin.Text
+                                            },
+                                            {
+                                                "BMI", BMI.Text
+                                            },
+                                            {
+                                                "DiabetesPedigree", DiabitiesPedigree.Text
+                                            },
+                                            {
+                                                "Age", Age.Text
+                                            },
+                                }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                const string apiKey = "KOM6qoxLDuNUm7B1cFfQrfs+Y2Nxcqfu9uh/pvbYzNcUoEuE5L9BreQhVnjJNjJAcXn7EJj5ET+Y36WSpECgxg=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/a32cc0f6e4f4453fabf21b2a94d9a430/services/e439236284bf4efba856e42e023b7978/execute?api-version=2.0&format=swagger");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest).ConfigureAwait(false);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    FinalResult = result;
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        //private void FabOnClick(object sender, EventArgs eventArgs)
+        //{
+        //    View view = (View)sender;
+        //    Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+        //        .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        //}
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
